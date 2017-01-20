6a9664aee68f0b3a56551eadc019ae016456ed4b
dani <dani.zaghian@gmail.com>
2017-01-19 18:38:28 -0800

    added second query parameter

--- app/controllers/params_examples_controller.rb
+++ app/controllers/params_examples_controller.rb
@@ -2,6 +2,7 @@ class ParamsExamplesController < ApplicationController
 
   def query_params_method
     @message = params[:my_message]
+    @second_message = params[:second_message]
     render "query_params.html.erb"
   end
 
