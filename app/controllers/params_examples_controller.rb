9a754d98e9fe21b15cdf734ba673ef9da895dadb
dani <dani.zaghian@gmail.com>
2017-01-19 19:18:02 -0800

    exercises added

--- app/controllers/params_examples_controller.rb
+++ app/controllers/params_examples_controller.rb
@@ -6,4 +6,31 @@ class ParamsExamplesController < ApplicationController
     render "query_params.html.erb"
   end
 
+  def name_method
+    @my_name = params[:my_name]
+    render "name.html.erb"
+  end
+
+  def number_method
+    decided_number = 45
+    @guessed_number = params[:guess].to_i
+    if @guessed_number > decided_number
+      @message = "You are too high"
+    elsif 
+      @guessed_number < decided_number
+      @message = "You are too low"
+    else
+      @message = "You are correct"
+    end
+    render "number.html.erb"
+  end
+
+
+
+
+
+
+
+
+
 end
